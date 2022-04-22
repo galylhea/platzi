f82510d608faf62a3339ed2e98c38315691fcb7b
María Luisa Vides <noa.galilea@gmail.com>
2022-04-21 20:32:43 -0500

    Clase #3

--- my-store/src/app/app.module.ts
+++ my-store/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule }  from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImgComponent } from './components/img/img.component';
@@ -12,7 +12,8 @@ import { ImgComponent } from './components/img/img.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
