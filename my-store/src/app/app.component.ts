d29e671ad4336b7ae405a8fe112aee17271fe3b9
María Luisa Vides <noa.galilea@gmail.com>
2022-04-25 19:03:56 -0500

    Clase #5

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   imgParent = 'https://picsum.photos/200';
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'Product 1',
+      image: 'https://picsum.photos/700',
+      price: 100
+    },
+    {
+      id: '2',
+      name: 'Product 2',
+      image: 'https://picsum.photos/600',
+      price: 100
+    },
+    {
+      id: '3',
+      name: 'Product 3',
+      image: 'https://picsum.photos/500',
+      price: 100
+    },
+    {
+      id: '4',
+      name: 'Product 4',
+      image: 'https://picsum.photos/400',
+      price: 100
+    }
+  ];
 
   onLoaded(img: string): void {
     console.log('log padre', img);
